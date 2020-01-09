915c5b683eba9bdf1ae6d40187b99c62da7342e9
이한영 <dev@lhy.kr>
2020-01-09 15:38:23 +0900

    LoginForm의 login, clean메서드 구현

--- app/members/views.py
+++ app/members/views.py
@@ -20,14 +20,9 @@ def login_view(request):
     로그인에 실패하면 다시 로그인페이지로 이동
     """
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        print('username:', username)
-        print('password:', password)
-        user = authenticate(request, username=username, password=password)
-        print('user:', user)
-        if user:
-            login(request, user)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            form.login(request)
             return redirect('posts:post-list')
         else:
             return redirect('members:login')
