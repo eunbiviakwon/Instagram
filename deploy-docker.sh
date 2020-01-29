aee444d4f48ad331dc7fd00a51db9e8d1f31599c
이한영 <dev@lhy.kr>
2020-01-29 13:57:23 +0900

    deploy-docker.sh완성

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 IDENTITY_FILE="$HOME/.ssh/wps12th.pem"
 USER="ubuntu"
-HOST="15.164.222.130"
+HOST="13.209.96.163"
 TARGET=${USER}@${HOST}
 ORIGIN_SOURCE="$HOME/projects/fastcampus/12th/instagram/"
-DEST_SOURCE="/srv/"
+DOCKER_REPO="azelf/wps-instagram"
 SSH_CMD="ssh -i ${IDENTITY_FILE} ${TARGET}"
 
 echo "== Docker 배포 =="
@@ -12,11 +12,34 @@ echo "== Docker 배포 =="
 # 서버 초기설정
 echo "apt update & upgrade & autoremove"
 ${SSH_CMD} -C 'sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y && apt -y autoremove'
-echo "apt install python3-pip"
-${SSH_CMD} -C 'sudo apt -y install python3-pip'
+echo "apt install docker.io"
+${SSH_CMD} -C 'sudo apt -y install docker.io'
 
 # pip freeze
 echo "pip freeze"
-"$HOME"/.pyenv/versions/3.7.5/envs/wps-instagram/bin/pip freeze > "$HOME"/projects/fastcampus/12th/instagram/requirements.txt
+"$HOME"/.pyenv/versions/3.7.5/envs/wps-instagram/bin/pip freeze > "${ORIGIN_SOURCE}"requirements.txt
+
+# docker build
+echo "docker build"
+docker build -q -t ${DOCKER_REPO} -f Dockerfile "${ORIGIN_SOURCE}"
+
+# docker push
+echo "docker push"
+docker push ${DOCKER_REPO}
+
+echo "docker stop"
+${SSH_CMD} -C "sudo docker stop instagram"
+
+echo "docker pull"
+${SSH_CMD} -C "sudo docker pull ${DOCKER_REPO}"
+
+# screen에서 docker run
+echo "screen settings"
+# 실행중이던 screen 세션종료
+${SSH_CMD} -C 'screen -X -S docker quit'
+# screen 실행
+${SSH_CMD} -C 'screen -S docker -d -m'
+# 실행중인 세션에 명령어 전달
+${SSH_CMD} -C "screen -r docker -X stuff 'sudo docker run --rm -it -p 80:8000 --name=instagram azelf/wps-instagram\n'"
 
 echo "  finish!"
